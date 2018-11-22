ddb94ffbabfd2720c0ba9799743334826708a300
Sarah Lawrence <Sarah.Lawrence@mi9.com.au>
2018-11-22 16:04:45 +1100

    init

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import logo from "../../images/logo.png";
+
+export default () => {
+  return (
+    <div className="header">
+      <img src={logo} className="logo" />
+    </div>
+  );
+};
