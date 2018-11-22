c2d48c77d20fc5c836c686d4dfdf7c7cbfbf55c0
Sarah Lawrence <s.j.lawrence8@gmail.com>
2018-11-22 17:30:27 +1100

    homepage styling

--- src/components/LocationButton/index.js
+++ src/components/LocationButton/index.js
@@ -4,9 +4,9 @@ import "./style.css";
 
 export default props => {
   return (
-    <div className="location-button">
+    <button className="location-button">
       <img src={props.image} className="location-image" alt={props.text} />
       <p className="location-text">{props.text}</p>
-    </div>
+    </button>
   );
 };
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -3,21 +3,32 @@ import React from "react";
 import LocationButton from "../../components/LocationButton/index";
 import "./style.css";
 
-export default () => {
+import ASQ from "../../images/ASQ.png";
+import DEN from "../../images/DEN.png";
+import TCN from "../../images/TCN.png";
+
+export default props => {
   return (
     <div className="home">
       <div>
-        <button className="button-use-my-location">
+        <button
+          className="button-use-my-location"
+          onClick={() => handleClick(props)}
+        >
           Use my current location
         </button>
       </div>
       <div className="divider" />
       <div className="button-group-label">Select your location</div>
       <div className="button-group">
-        <LocationButton text="Australia Square" />
-        <LocationButton text="TCN Willoughby" />
-        <LocationButton text="Dennison Street" />
+        <LocationButton text="Australia Square" image={ASQ} />
+        <LocationButton text="TCN Willoughby" image={TCN} />
+        <LocationButton text="Dennison Street" image={DEN} />
       </div>
     </div>
   );
+};
+
+const handleClick = props => {
+  console.log(props);
 };
